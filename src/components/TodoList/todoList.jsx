17d653a0675e64d0055158f0269a4e4097605b4d
ZiyouddinMominov <muminovz2009@gmail.com>
2024-02-03 00:15:04 +0500

    finished

--- src/components/TodoList/todoList.jsx
+++ src/components/TodoList/todoList.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Todo from "../Todo/todo";
 function TodoList({ todos, onDelete }) {
   return (
-    <div>
+    <>
       {todos.map((todo, index) => (
         <Todo key={index} todo={todo} onDelete={() => onDelete(index)} />
       ))}
-    </div>
+    </>
   );
 }
 
